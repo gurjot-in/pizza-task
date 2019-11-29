685a96c06d24f72ee0ef6cedbff0239eaa0026c0
gurjot-in <gurjot@bluestacks.com>
2019-11-29 22:59:18 +0530

    write unit test cases covering all given points in email

--- pizza_orders/urls.py
+++ pizza_orders/urls.py
@@ -3,7 +3,7 @@ from pizza_orders import views
 from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 
-router.register(r'customers', views.CustomerViewSet)
-router.register(r'orders', views.OrderViewset)
-router.register(r'order_items', views.OrderItemViewset)
+router.register(r'customers', views.CustomerViewSet, base_name= 'customers')
+router.register(r'orders', views.OrderViewset, base_name = 'orders')
+router.register(r'order_items', views.OrderItemViewset, base_name = 'order_items')
 urlpatterns = router.urls
